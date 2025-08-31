a737c7c34b4438042ded0e98dd4f7fd9a913b3b6
Sankyjz <santiagoje0306@gmail.com>
2025-08-31 15:31:29 -0600

    clase medicamento

--- src/main/java/Proyecto/Logic/Medicamento.java
+++ src/main/java/Proyecto/Logic/Medicamento.java
@@ -1,4 +1,37 @@
 package Proyecto.Logic;
 
 public class Medicamento {
+    protected String codigo;
+    protected String nombre;
+    protected int presentacion; //cantidad
+
+    public Medicamento(String codigo, String nombre, int presentacion) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.presentacion = presentacion;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPresentacion() {
+        return presentacion;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPresentacion(int presentacion) {
+        this.presentacion = presentacion;
+    }
 }
